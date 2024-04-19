e9742e1b57d2bd934b1d180e842fa895c84e8e9d
ottav14 <dominicjottaviano@gmail.com>
2024-04-18 21:41:12 -0400

    started project..

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 
 int main() {
 
+  // Initialize variables
+  int window_width = 1920;
+  int window_height = 1080;
+  double time = 0.0;
+  int resolution = 50;
+
   // Initialize framework
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
   SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_CreateWindowAndRenderer(1920, 1080, 0, &window, &renderer);
-
-
-  // Initialize variables
-  double time = 0.0;
+  SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window, &renderer);
 
   // Main loop
   bool quit = false;
@@ -24,14 +26,17 @@ int main() {
     }
 
     // Draw background
-    SDL_Rect bg = {0, 0, 1920, 1080};
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); 
+    SDL_Rect bg = {0, 0, window_width, window_height};
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
     SDL_RenderFillRect(renderer, &bg);
 
-    // Draw a red rectangle
-    SDL_Rect rect = {100 + (int) time, 100, 200, 150};
+    // Draw lines
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
-    SDL_RenderFillRect(renderer, &rect);
+    for(int i=0;i<resolution;i++) {
+      for(int j=0;j<resolution;j++) {
+	SDL_RenderDrawLine(renderer, i * window_width / resolution, j * window_height / resolution, 100, 100);
+      }
+    }
 
     // Update the window
     SDL_RenderPresent(renderer);
